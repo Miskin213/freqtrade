8e1802bd0b34542dbdeea75c374a102dba0d8b23
Miskin <111906147+Miskin213@users.noreply.github.com>
2026-03-12 14:13:02 +0300

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo chown -R ftuser:ftuser /freqtrade/user_data
-mkdir -p /freqtrade/user_data/strategies
-echo $FREQTRADE_CONFIG > /freqtrade/user_data/config.json
-cp /freqtrade/freqtrade/templates/sample_strategy.py /freqtrade/user_data/strategies/SampleStrategy.py
-freqtrade trade --config /freqtrade/user_data/config.json --strategy SampleStrategy
